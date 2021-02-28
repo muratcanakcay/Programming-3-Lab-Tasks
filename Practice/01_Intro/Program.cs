fd7525e3ba620b9930225be4cf251cd89468b081
Muratcan Akçay <muratcanakcay@gmail.com>
2021-02-28 05:42:36 +0100

    Init

---
+++ Practice/01_Intro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System; // static is not used because System is a namespace
+using static System.Console; // static is used because Console is a type
+
+class GlobalClass { }
+
+
+namespace Intro
+{
+
+    class Test
+    {
+        void func()
+        {
+            int a = 1;
+            int b, c, d;
+            int[] t1, t2;
+            b = 2;
+            this.e = 0; // instance.type
+            Intro.Test.f = 0; // namespace.type.type
+            if (a < 2) c = 3;
+            t1 = new int[5];
+            d = a;
+            d = b;
+
+            d = t1[0];
+            //d = t2[0]; // error - not assigned
+        }
+
+        int e;
+        static int f;
+        global::GlobalClass GC; // from global namespace
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Practice/02_Classes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Classes
+{
+    // static class
+    public static class A
+    {
+        // only static members
+        static public int staticMember;
+        // int public nonStaticMember; // gives error
+    } // class A
+
+    // "ordinary" class
+    class B 
+    {
+        public int n;
+        public static int x;
+        
+        public void fun1(B b)
+        {
+            n = b.n; // same as this.n = b.n
+            B.x = 0; // same as x = 0;
+        }
+        
+        public static void fun2(int i)
+        {
+            x = i; // same as B.x=i;
+        }
+        
+        public void fun3(B b)
+        {
+            // B.n = 1; // error, n is instance member  - requires instance reference (this.x = ...)
+            this.n = 1; // correct
+            
+            // b.x = 2; // error, x is static member - requires class name reference (B.x = ...)
+            // correct in C++
+            B.x = 2; // correct
+        }
+    } // class B
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
